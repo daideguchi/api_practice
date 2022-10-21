d3ecb9774667e12014d694cb5778701b60beb223
daideguchi <dd.1107.11107@gmail.com>
2022-10-21 18:52:12 +0900

    221021

---
+++ practice1/cmc.py
@@ -0,0 +1,40 @@
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+
+
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+fiat = 'USD'
+
+API_KEY = 'eccd075b-7c11-428c-8aa3-92ed39f4aa61' # Set your api key
+
+headers = {
+  'Accepts': 'application/json',
+  'X-CMC_PRO_API_KEY': API_KEY,
+}
+
+params = {
+  'start':'1',
+  'limit':'3',
+  'convert':fiat
+}
+
+session = Session()
+session.headers.update(headers)
+
+try:
+  r = session.get(url, params=params)
+except (ConnectionError, Timeout, TooManyRedirects) as e:
+  print(e)
+
+print(r.json())
+
+import pandas as pd
+
+df = pd.DataFrame(r.json()['data'])
+df.head()
+
+f_name = 'cmc_coinlist.json'
+df.to_json(f_name)
+
+for target_key in df.quote.iloc[0][fiat]:
+    df[fiat+'_'+target_key] = df.quote.map(lambda d: d[fiat][target_key])
